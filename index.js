4b83148d8e54a7da2e7457ecb6336d407d639d18
Eder Canada <canada.eder@gmail.com>
2020-07-13 16:35:49 -0700

    const

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const greet = "hello world"
+/////////////////////////////CONST/////////////////////////
+// const cannot change value, unlike var, and once I declare a const variable I have to assign it a value, it can even be an empty object{}
+
+const x = 2
+console.log(x)
+
+// I
+const y = {}
+y['a'] = 1
+console.log(y)
