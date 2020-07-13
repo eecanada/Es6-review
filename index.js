ed2c05d12d055f21ac445282bb93f41eef89f8f5
Eder Canada <canada.eder@gmail.com>
2020-07-13 16:42:13 -0700

    let

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-/////////////////////////////CONST/////////////////////////
+/////////////////////////////////////////////////////CONST////////////////////////////////////////////////////////
 // const cannot change value, unlike var, and once I declare a const variable I have to assign it a value, it can even be an empty object{}
 
 const x = 2
 console.log(x)
 
-// I
 const y = {}
 y['a'] = 1
 console.log(y)
+
+
+///////////////////////////////////////////////////////LET////////////////////////////////////////////////////////
+
+// the difference betwwen let and const is the the let variables value can change, however I cannot redefine x again like with var
+let x = 1 
+x = 3
+let x = 2 // error
