657d8d3a5640ed5a91fc645203bce5c5fe465a89
Eder Canada <canada.eder@gmail.com>
2020-08-09 22:23:14 -0700

    arrow functions

--- index.js
+++ index.js
@@ -1,52 +1,97 @@
 /////////////////////////////////////////////////////CONST////////////////////////////////////////////////////////
 // const cannot change value, unlike var, and once I declare a const variable I have to assign it a value, it can even be an empty object{}
 
-var x = 1  // I can keep chnageing it to anything I want
+// var x = 1  // I can keep chnageing it to anything I want
 
-const x = 2
-console.log(x) // --> 2
+// const x = 2
+// console.log(x) // --> 2
 
-const y = {}
-y['a'] = 1
-console.log(y)
-
-
-
-///////////////////////////////////////////////////////LET////////////////////////////////////////////////////////
-
-// the difference betwwen let and const is the the let variables value can be assigned multiple times, but I cannot keep declaring it, with var I can delare a variable many times and it will still output a value
-let x = 1 
-x = 3
-let x = 2 // error
-
-let x; // I can even declare x without assiging it a value, but if I were to do that with const it would not work
-
-var y = 1
-var y = 3 // still works 
+// const y = {}
+// y['a'] = 1
+// console.log(y)
 
 
 
+// ///////////////////////////////////////////////////////LET////////////////////////////////////////////////////////
+
+// // the difference between let and const is that let variables value can be assigned multiple times, but I cannot keep declaring it, with var I can delare a variable many times and it will still output a value
+// let x = 1 
+// x = 3
+// let x = 2 // error
+
+// let x; // I can even declare x without assiging it a value, but if I were to do that with const it would not work
+
+// var y = 1
+// var y = 3 // still works 
 
 
-///////////////////////////////////////////////////////SCOPE//////////////////////////////////////////////////////
-console.log(x) // temperal dead zone, cant access x before it has been declared
-
-const x = 1 
 
 
-// the x is in the if block and only accessible there
-if(true){ 
-  const x = 1 // const and let are not hoisted
-  console.log(x)
-}
 
-{
-  let x = 1 
-  console.log(x) //1
-  {
-    let x = 2 
-    console.log(x) // 2
+// ///////////////////////////////////////////////////////SCOPE//////////////////////////////////////////////////////
+// console.log(x) // temperal dead zone, cant access x before it has been declared
+
+// const x = 1 
+
+
+// // the x is in the if block and only accessible there
+// if(true){ 
+//   const x = 1 // const and let are not hoisted
+//   console.log(x)
+// }
+
+// {
+//   let x = 1 
+//   console.log(x) //1
+//   {
+//     let x = 2 
+//     console.log(x) // 2
+//   }
+// }
+
+// // function declaration and expressions would work the same as const and let
+
+
+
+///////////////////////////////////////////////////////ARROW FUNCTION/////////////////////////////////////////////
+ 
+const arr = [1,2,3,4]
+
+//Statement Bodies 
+const byTwo = arr.map((number)=>{
+  return number * 2 
+})
+
+console.log(byTwo) // ->  2, 4, 6, 8 ]
+
+//Expression Bodies
+const arr1 = [2,3,4,5]
+const byThree = arr1.map(number => number *3)
+console.log(byThree)
+
+
+var obj = {
+  value: 0,
+  increment:function(){
+    setTimeout(function(){
+      this.value++ // Nan, because this is referring to the function scope setTimeOut and not key of the object
+      console.log(this.value)
+    }, 1000)
   }
 }
 
-// function declaration and expressions would work the same as const and let
+obj.increment()
+
+// Lexical This - use arrow functions 
+var obj1 = {
+  value: 0,
+  increment: function () {
+    setTimeout(() =>{
+      this.value++
+      console.log(this.value)
+    }, 1000)
+    }
+}
+
+obj1.increment()
+
